e59d59328f872cf51ed884614ca43a1c7a0e4c3a
CrioUser <crio-user@criodo.com>
2023-07-25 17:34:47 +0530

    ME_QMONEY_V2_MODULE_REST_API

--- qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -272,7 +272,7 @@ public class PortfolioManagerApplication {
     Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler());
     ThreadContext.put("runId", UUID.randomUUID().toString());
 
-   // printJsonObject(mainReadFile(args));
+    printJsonObject(mainReadFile(args));
 
 
     printJsonObject(mainReadQuotes(args));
